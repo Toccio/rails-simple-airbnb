0eff1bb8b8cc1536f2f3ee2c98727f78eeebf5ba
luca visaggio <luca.t.visaggio@gmail.com>
2022-07-22 08:53:28 +0200

    show created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaning up database"
+Flat.destroy_all
+puts "database clean"
+
+puts 'creating flats'
+  4.times do
+    flat = Flat.create(
+      name: Faker::Games::Pokemon.name,
+      address: Faker::Games::Pokemon.location,
+      price_per_night: "300 $",
+      number_of_guests: 4
+    )
+    puts "Flat #{flat.id} created"
+  end
+puts 'Done'
